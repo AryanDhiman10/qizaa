c03ddea8807c8cccddc1b15806658cdbcdab87a7
ASGDMN <97045861+ASGDMN@users.noreply.github.com>
2022-05-24 21:13:52 +0530

    Final changes to Join Us

--- src/components/JoinUs.jsx
+++ src/components/JoinUs.jsx
@@ -5,24 +5,26 @@ import Scale from '../assets/JoinUs/Scale.png'
 
 const JoinUs = () => {
     return(
-        <div className="container shadow-lg p-3 mb-5 bg-white rounded text-center">
-            <h1 className='Display-2 py-4' style={{color: "rgb(106, 190, 82)"}}>Live a Sustainable Life</h1>
-            <h4 className='py-3'>Join Us Now</h4>
-            <input className='px-4' type="email" placeholder='Add your Email' style={{border: "2px solid grey"}} />
-            <br />
-            <button className='btn px-4 my-4' style={{borderRadius: '40px', backgroundColor: "rgb(106, 190, 82)", color: "white"}}>Submit</button>
-            <div className="d-flex justify-content-around pb-5">
-                <div class="vstack gap-2 col-md-5 mx-auto">
-                    <h3 className='display-6'>Create</h3>
-                    <img className='mx-auto' src={Create} alt="Create"style={{maxWidth: "176px", maxHeight: "176px"}} />
-                </div>
-                <div class="vstack gap-2 col-md-5 mx-auto">
-                    <h3 className='display-6'>Innovate</h3>
-                    <img className='mx-auto' src={Innovate} alt="Create"style={{maxWidth: "176px", maxHeight: "176px"}} />
-                </div>
-                <div class="vstack gap-2 col-md-5 mx-auto">
-                    <h3 className='display-6'>Scale</h3>
-                    <img className='mx-auto' src={Scale} alt="Create"style={{maxWidth: "176px", maxHeight: "176px"}} />
+        <div className="pb-2 pt-5">
+            <div className="container shadow-lg p-3 mb-5 bg-white rounded text-center">
+                <h1 className='Display-2 py-4' style={{color: "rgb(106, 190, 82)"}}>Live a Sustainable Life</h1>
+                <h4 className='py-3'>Join Us Now</h4>
+                <input className='px-4' type="email" placeholder='Add your Email' style={{border: "2px solid grey"}} />
+                <br />
+                <button className='btn px-4 my-4' style={{borderRadius: '40px', backgroundColor: "rgb(106, 190, 82)", color: "white"}}>Submit</button>
+                <div className="d-flex justify-content-around pb-5">
+                    <div class="vstack gap-2 col-md-5 mx-auto">
+                        <h3 className='display-6'>Create</h3>
+                        <img className='mx-auto' src={Create} alt="Create"style={{maxWidth: "176px", maxHeight: "176px"}} />
+                    </div>
+                    <div class="vstack gap-2 col-md-5 mx-auto">
+                        <h3 className='display-6'>Innovate</h3>
+                        <img className='mx-auto' src={Innovate} alt="Create"style={{maxWidth: "176px", maxHeight: "176px"}} />
+                    </div>
+                    <div class="vstack gap-2 col-md-5 mx-auto">
+                        <h3 className='display-6'>Scale</h3>
+                        <img className='mx-auto' src={Scale} alt="Create"style={{maxWidth: "176px", maxHeight: "176px"}} />
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/SustainableBrands.jsx
+++ src/components/SustainableBrands.jsx
@@ -10,15 +10,17 @@ import SustainableBrands6 from '../assets/SustainbleBrands6.webp';
 
 const SustainableBrands = () => {
     return(
-        <div className="container text-center">
-            <h1 className="display-4" style={{marginTop: "1rem", margin: "2rem"}}>Sustainable Brands</h1>
-            <div className="card-group d-flex justify-content-around">
-                <Card image={SustainableBrands1} title="Brown Living" details="Buy sustainable zero-waste products from best brands" />
-                <Card image={SustainableBrands2} title="Planet Care" details="Buy sustainable zero-waste products from best brands." />
-                <Card image={SustainableBrands3} title="Prakati" details="Buy sustainable zero-waste products from best brands" />
-                <Card image={SustainableBrands4} title="Ecohoy" details="Buy sustainable zero-waste products from best brands." />
-                <Card image={SustainableBrands5} title="The Sustainable Indian" details="Buy sustainable zero-waste products from best brands." />
-                <Card image={SustainableBrands6} title=" Earthsy.in" details="Buy sustainable zero-waste products from best brands" />
+        <div  style={{backgroundColor: "rgb(250, 250, 250)"}}>
+            <div className="container text-center py-1">
+                <h1 className="display-4" style={{marginTop: "1rem", margin: "2rem"}}>Sustainable Brands</h1>
+                <div className="card-group d-flex justify-content-around">
+                    <Card image={SustainableBrands1} title="Brown Living" details="Buy sustainable zero-waste products from best brands" />
+                    <Card image={SustainableBrands2} title="Planet Care" details="Buy sustainable zero-waste products from best brands." />
+                    <Card image={SustainableBrands3} title="Prakati" details="Buy sustainable zero-waste products from best brands" />
+                    <Card image={SustainableBrands4} title="Ecohoy" details="Buy sustainable zero-waste products from best brands." />
+                    <Card image={SustainableBrands5} title="The Sustainable Indian" details="Buy sustainable zero-waste products from best brands." />
+                    <Card image={SustainableBrands6} title=" Earthsy.in" details="Buy sustainable zero-waste products from best brands" />
+                </div>
             </div>
         </div>
     )
